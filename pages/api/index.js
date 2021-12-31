86ad3fb373e57681bc76ffc56e1fe4405d282013
Oskar Rough <oskar@rough.dk>
2021-12-31 19:14:30 +0100

    Refactor around environment variables

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,18 +1,22 @@
 import config from '../../utils/config'
 
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 export default function handler(req, res) {
 	const welcome = {
-		message: 'Welcome to the Radio4000 api',
-		databaseDocumentationUrl: 'https://github.com/internet4000/radio4000-firebase-rules',
-		apiDocumentationUrl: 'https://github.com/internet4000/radio4000-api-vercel',
-		databaseUrl: config.databaseURL,
-		channelsUrl: `${config.databaseURL}channels.json`,
-		channelUrl: `${config.databaseURL}channels/{id}.json`,
-		tracksUrl: `${config.databaseURL}tracks.json`,
-		trackUrl: `${config.databaseURL}tracks/{id}.json`,
-		channelEmbedUrl: `${config.apiURL}/embed?slug={slug}`,
-		channelOEmbedUrl: `${config.apiURL}/oembed?slug={slug}`,
+		message: 'Welcome to the Radio4000 API',
+		api: {
+			docs: 'https://github.com/internet4000/radio4000-api-vercel',
+			channelBackup: `${config.apiURL}/api/backup?slug={slug}`,
+			channelEmbedUrl: `${config.apiURL}/api/embed?slug={slug}`,
+			channelOEmbedUrl: `${config.apiURL}/api/oembed?slug={slug}`,
+		},
+		database: {
+			docs: 'https://github.com/internet4000/radio4000-firebase-rules',
+			url: config.databaseURL,
+			channelsUrl: `${config.databaseURL}channels.json`,
+			channelUrl: `${config.databaseURL}channels/{id}.json`,
+			tracksUrl: `${config.databaseURL}tracks.json`,
+			trackUrl: `${config.databaseURL}tracks/{id}.json`,
+		},
 	}
 	res.status(200).json(welcome)
 }
--- utils/config.js
+++ utils/config.js
@@ -1,23 +1,12 @@
-// const {NODE_ENV} = process.env
-
-// URL of this application
-let apiURL = 'http://localhost:4001'
-
-// Firebase database URL
-let databaseURL = 'https://radio4000-staging.firebaseio.com/'
-
-// CDN URL to the radio4000-player script
-const playerScriptURL = 'https://cdn.jsdelivr.net/npm/radio4000-player@latest/dist/radio4000-player.min.js'
-
-// if (NODE_ENV === 'production') {
-// 	apiURL = 'https://api.radio4000.com'
-// 	databaseURL = 'https://radio4000.firebaseio.com/'
-// }
+const production = process.env.VERCEL_ENV === 'production'
 
 const config = {
-	apiURL,
-	databaseURL,
-	playerScriptURL,
+	// URL of this application
+	apiURL: process.env.VERCEL_URL || 'http://localhost:3000',
+	// Firebase database URL
+	databaseURL: production ? 'https://api.radio4000.com' : 'https://radio4000-staging.firebaseio.com/',
+	// CDN URL to the radio4000-player script
+	playerScriptURL: 'https://cdn.jsdelivr.net/npm/radio4000-player@latest/dist/radio4000-player.min.js',
 }
 
 export default config
