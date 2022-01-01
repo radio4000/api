5b12cecb44f7a3b2a92f9a092fb53bfe4e865e31
hugur <hugu.r.p+hello@pm.me>
2022-01-01 14:42:58 +0100

    fix databaseUrl when production

--- utils/config.js
+++ utils/config.js
@@ -4,7 +4,7 @@ const config = {
 	// URL of this application
 	apiURL: process.env.VERCEL_URL || 'http://localhost:3000',
 	// Firebase database URL
-	databaseURL: production ? 'https://api.radio4000.com' : 'https://radio4000-staging.firebaseio.com/',
+	databaseURL: production ? 'https://radio4000.firebaseio.com/' : 'https://radio4000-staging.firebaseio.com/',
 	// CDN URL to the radio4000-player script
 	playerScriptURL: 'https://cdn.jsdelivr.net/npm/radio4000-player@latest/dist/radio4000-player.min.js',
 }
