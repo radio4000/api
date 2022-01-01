f964ce7c5552162cc4a4030d79690328c157f4f4
Oskar Rough <oskar@rough.dk>
2022-01-01 12:31:55 +0100

    Only run JSDOM if we have a slug

--- pages/api/embed.js
+++ pages/api/embed.js
@@ -5,15 +5,12 @@ import noEndpoint from '../../utils/no-endpoint'
 import getIframe from '../../utils/get-iframe'
 
 export default function handler(req, res) {
-	// Prepare stuff for XSS
-	const window = new JSDOM('').window
-	const DOMPurify = createDOMPurify(window)
-
 	const slug = req.query.slug
-
 	if (!slug) return noEndpoint(res)
 
 	// Prevent XSS
+	const DOMPurify = createDOMPurify(new JSDOM('').window)
 	const safeSlug = DOMPurify.sanitize(slug)
+
 	res.status(200).send(getIframe(safeSlug, config.playerScriptURL))
 }
