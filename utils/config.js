3d0d6d0c0d3decc0344833f03fca98c36e055997
hugur <hugu.r.p+hello@pm.me>
2022-01-01 14:52:38 +0100

    fix environements

--- utils/config.js
+++ utils/config.js
@@ -1,12 +1,21 @@
-const production = process.env.VERCEL_ENV === 'production'
-
+// default case, process.env.VERCEL_ENV === 'development'
 const config = {
 	// URL of this application
-	apiURL: process.env.VERCEL_URL || 'http://localhost:3000',
+	apiURL: 'http://localhost:3000',
 	// Firebase database URL
-	databaseURL: production ? 'https://radio4000.firebaseio.com/' : 'https://radio4000-staging.firebaseio.com/',
+	databaseURL: 'https://radio4000-staging.firebaseio.com/',
 	// CDN URL to the radio4000-player script
 	playerScriptURL: 'https://cdn.jsdelivr.net/npm/radio4000-player@latest/dist/radio4000-player.min.js',
 }
+
+if (process.env.VERCEL_ENV === 'production') {
+	config.apiURL = 'https://api.radio4000.com'
+	config.databaseURL = 'https://radio4000.firebaseio.com/'
+}
+
+if (process.env.VERCEL_ENV === 'preview') {
+	if (process.env.VERCEL_URL) config.apiURL = process.env.VERCEL_URL
+}
+
 
 export default config
