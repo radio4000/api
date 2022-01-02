f5f03c2d037c43a028a2bbafba87802d0cc21389
Oskar Rough <oskar@rough.dk>
2022-01-02 16:38:21 +0100

    Start validating Supabase token as well

--- pages/api/migrate.js
+++ pages/api/migrate.js
@@ -1,49 +1,49 @@
 import firebaseAdmin from '../../utils/firebase-admin'
 import {getAuth} from 'firebase-admin/auth'
+import supabase from '../../utils/supabase'
 
 /*
 	Accepts a body with tokenFirebase & tokenSupabase,
 	and verifies both. If both are valid, we call the migration.
 
 	To test the endpoint, use this:
-
 curl -X POST http://localhost:3000/migrate -d '{"tokenFirebase":"value1", "tokenSupabase":"value2"}' -H "Content-Type: application/json" -i
-
 */
 export default async function migrate(req, res) {
 	// Only allow POST requests.
 	if (req.method !== 'POST') {
 		return res.status(400).send({message: 'Only POST requests allowed'})
 	}
+
 	// Collect firebase + supabase tokens from the request
 	const {tokenFirebase, tokenSupabase} = req.body
 	if (!tokenFirebase || !tokenSupabase) {
-		return res.status(403).send('Missing token')
+		return res.status(403).send({message: 'Missing token'})
 	}
+
 	// Verify both
 	try {
 		const firebaseIsValid = await verifyFirebaseToken(tokenFirebase)
 		const supabaseIsValid = await verifySupabaseToken(tokenSupabase)
 		console.log(firebaseIsValid, supabaseIsValid)
-		res.status(200).send('tokens are here and valid!')
+		if (firebaseIsValid && supabaseIsValid) {
+			res.status(200).send({message: 'Tokens are valid'})
+		} else {
+			res.status(403).send({message: 'Tokens are not valid'})
+		}
 	} catch (err) {
 		console.log(err)
-		res.status(403).send('tokens are here but not valid')
+		res.status(403).send({message: 'Could not verify tokens'})
 	}
 }
 
 function verifyFirebaseToken(idToken) {
-	// return Promise.resolve(false)
 	return getAuth().verifyIdToken(idToken)
-		// .then((decodedToken) => {
-		// 	const uid = decodedToken.uid
-		// 	console.log(uid)
-		// 	return uid
-		// 	// ...
-		// })
 }
 
-function verifySupabaseToken(token) {
-	return Promise.resolve(false)
+async function verifySupabaseToken(access_token) {
+	console.log(access_token)
+	const {user, data, error} = await supabase.auth.api.getUser(access_token)
+	console.log(user, data, error)
+	return false
 }
-
