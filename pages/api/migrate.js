38a06f02857ee982b381f14e27b89ca62118759f
Oskar Rough <oskar@rough.dk>
2022-01-02 13:41:36 +0100

    Prepare api/migrate endpoint

--- pages/api/migrate.js
+++ pages/api/migrate.js
@@ -1,59 +1,49 @@
 import firebaseAdmin from '../../utils/firebase-admin'
+import {getAuth} from 'firebase-admin/auth'
+
 /*
-	Accepts a body with
-	- tokenFirebase
-	- tokenSupabase
-	get userId{Firebase,Supabase} from token{Firebase,Supabase}
-	get channelId from userId{Firebase,Supabase}
-	with tracks from channelIdFirebase, add tracks to channelIdSupabase
+	Accepts a body with tokenFirebase & tokenSupabase,
+	and verifies both. If both are valid, we call the migration.
+
+	To test the endpoint, use this:
+
+curl -X POST http://localhost:3000/migrate -d '{"tokenFirebase":"value1", "tokenSupabase":"value2"}' -H "Content-Type: application/json" -i
+
 */
 export default async function migrate(req, res) {
+	// Only allow POST requests.
 	if (req.method !== 'POST') {
-		res.status(400).send({message: 'Only POST requests allowed'})
-		return
+		return res.status(400).send({message: 'Only POST requests allowed'})
 	}
 	// Collect firebase + supabase tokens from the request
-	// const body = JSON.parse(req.body)
-	// console.log(body)
 	const {tokenFirebase, tokenSupabase} = req.body
-
+	if (!tokenFirebase || !tokenSupabase) {
+		return res.status(403).send('Missing token')
+	}
 	// Verify both
-	const firebaseIsValid = await verifyFirebaseToken(tokenFirebase)
-	const supabaseIsValid = await verifySupabaseToken(tokenSupabase)
-	console.log(firebaseIsValid, supabaseIsValid)
-	const bothTokensAreValid = tokenFirebase && tokenSupabase
-
-	if (bothTokensAreValid) {
-		res.status(200).send('all good')
-	} else {
-		res.status(403).send('not so good')
+	try {
+		const firebaseIsValid = await verifyFirebaseToken(tokenFirebase)
+		const supabaseIsValid = await verifySupabaseToken(tokenSupabase)
+		console.log(firebaseIsValid, supabaseIsValid)
+		res.status(200).send('tokens are here and valid!')
+	} catch (err) {
+		console.log(err)
+		res.status(403).send('tokens are here but not valid')
 	}
 }
 
 function verifyFirebaseToken(idToken) {
 	// return Promise.resolve(false)
-	return getAuth()
-		.verifyIdToken(idToken)
-		.then((decodedToken) => {
-			const uid = decodedToken.uid
-			console.log(uid)
-			return uid
-			// ...
-		})
-		.catch((error) => {
-			console.log(error)
-			return false
-			// Handle error
-		})
+	return getAuth().verifyIdToken(idToken)
+		// .then((decodedToken) => {
+		// 	const uid = decodedToken.uid
+		// 	console.log(uid)
+		// 	return uid
+		// 	// ...
+		// })
 }
 
 function verifySupabaseToken(token) {
 	return Promise.resolve(false)
 }
-// const supabaseConfig = {
-// 	url: 'https://jbmaibztbxmtrtjzrory.supabase.co',
-// 	key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MDk1OTgxMSwiZXhwIjoxOTU2NTM1ODExfQ.s2Cr-3AoFxkzQwrPXEx8vcj7eJluK__VK8XKiXsCxT4',
-// }
 
-// // init supabase client
-// const supabase = createClient(supabaseConfig.url, supabaseConfig.key)
--- utils/firebase-admin.js
+++ utils/firebase-admin.js
@@ -1,17 +1,20 @@
 import admin from 'firebase-admin'
 import {initializeApp} from 'firebase-admin/app'
-// import {getAuth} from 'firebase-admin'
 
+// Instead of using a service account file, we load the three required keys
+// from the service account file as env vars.
 // const serviceAccount = require('path/to/serviceAccountKey.json')
 const projectId = process.env.FIREBASE_SERVICE_ACCOUNT_PROJECT_ID
-const clientEmail = process.env.FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY
-const privateKey = process.env.FIREBASE_SERVICE_ACCOUNT_CLIENT_EMAIL
+const clientEmail = process.env.FIREBASE_SERVICE_ACCOUNT_CLIENT_EMAIL
+const privateKey = process.env.FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY
 const formattedPrivateKey = privateKey.replace(/\\n/g, '\n')
+// console.log(privateKey)
+// console.log(formattedPrivateKey)
 
-initializeApp({
-	// credential: admin.credential.cert(serviceAccount),
+// Init Firebase Admin SDK
+const firebaseAdmin = initializeApp({
 	credential: admin.credential.cert({projectId, clientEmail, privateKey: formattedPrivateKey}),
 	databaseURL: 'https://radio4000.firebaseio.com',
 })
 
-export default admin
+export default firebaseAdmin
