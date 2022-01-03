ebb5e8d303e6276ae13b40718ce6de624cdeba5f
Oskar Rough <oskar@rough.dk>
2022-01-03 18:42:41 +0100

    More

--- middlewares/auth/test.js
+++ middlewares/auth/test.js
@@ -1,5 +1,5 @@
-// require a testToken authentication
-export const requireTestSession = (fn) => async (req, res) => {
+// require a Radio4000 anonymous token for authentication
+export const requireRadio4000Anon = (fn) => async (req, res) => {
 	const {tokenTest} = req.body
 	if (!tokenTest) {
 		return res.status(401).json({
@@ -19,7 +19,7 @@ export const requireTestSession = (fn) => async (req, res) => {
 	return fn(req, res)
 }
 
-const verifyTestToken = async (token) => {
+const verifyRadio4000AnonToken = async (token) => {
 	if (!token) {
 		Promise.reject({
 			message: 'You should use <any> token! Only empty is invalid'
