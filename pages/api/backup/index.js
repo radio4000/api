65e7bed25ad76717dec887ebb8ceda18cc944ca6
hugur <hugu.r.p+hello@pm.me>
2022-01-05 12:42:34 +0100

    ditch radio4000-sdk in favor of provider/firebase-admin for /backup

--- pages/api/backup/index.js
+++ pages/api/backup/index.js
@@ -1,17 +1,27 @@
-import {createBackup} from 'radio4000-sdk'
+import {getChannelBackup} from 'lib/providers/firebase-admin'
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
 	const slug = req.query.slug
-	if (!slug)
+	if (!slug) {
 		return res.status(404).json({
 			message: 'Missing parameter `?slug=` for a channel slug',
 		})
+	}
 
-	return createBackup(slug)
-		.then((backup) => res.status(200).send(backup))
-		.catch((error) => {
-			res.status(404).json({
-				message: `Could not fetch channel @${slug}`
-			})
+	let backup
+	try {
+		backup = await getChannelBackup(slug)
+	} catch(error) {
+		console.error(error)
+		res.status(404).json({
+			message: `Could find data to backup for channel @${slug}`
 		})
+	}
+
+	if (!backup) {
+		res.status(404).json({
+			message: `Could not create backup for channel @${slug}`
+		})
+	}
+	res.status(200).send(backup)
 }
