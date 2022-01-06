acc5d74313ee91519ba9245d09507dc2597dd3d3
Oskar Rough <oskar@rough.dk>
2022-01-06 21:59:46 +0100

    Add /youtube?ytid= endpoint

---
+++ pages/api/youtube.js
@@ -0,0 +1,55 @@
+import fetch from 'node-fetch'
+import config from 'lib/config'
+
+// const {asSeconds} = require('pomeranian-durations')
+
+const {YOUTUBE_API_KEY: key} = config
+
+export default async function handler(req, res) {
+	const ytid = req.query.ytid
+
+	try {
+		// Make sure we have what we need.
+		if (!key) throw new Error('A YOUTUBE_KEY in your .env file is required')
+		if (!ytid) throw new Error('A ytid query parameter is required')
+
+		// Fetch from the YouTube Data API. Key is from Google Cloud console credentials.
+		const data = await fetch(
+			`https://www.googleapis.com/youtube/v3/videos?part=status,contentDetails,snippet&id=${ytid}&key=${key}`
+		)
+		const json = await data.json()
+
+		// It does not throw with no results, so we check here.
+		if (json.items.length === 0) {
+			return res.status(404).json({message: 'YouToube video not found'})
+		}
+
+		// Serialize and return.
+		res.status(200).json(serialize(json.items[0]))
+	} catch (err) {
+		res.status(500).json({message: err.message})
+	}
+}
+
+// Picks the most useful things out of the default YouTube response.
+function serialize(item) {
+	return {
+		id: item.id,
+		url: `https://www.youtube.com/watch?v=${item.id}`,
+
+		// Requires ?part=snippet
+		title: item.snippet.title,
+		description: item.snippet.description,
+		thumbnails: item.snippet.thumbnails,
+		tags: item.snippet.tags,
+
+		// Requires ?part=contentDetails
+		// See https://github.com/date-fns/date-fns/pull/348
+		// Once date-fns supports durations let's switch to that.
+		// duration: asSeconds(item.contentDetails.duration),
+		duration: item.contentDetails.duration,
+
+		// Requires ?part=status
+		status: item.status,
+	}
+}
