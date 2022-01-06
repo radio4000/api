cfe0d33797d4c2d100a95ea0b49f1bcf49e8cd05
Oskar Rough <oskar@rough.dk>
2022-01-06 22:16:27 +0100

    Feedback

--- pages/api/youtube.js
+++ pages/api/youtube.js
@@ -1,31 +1,27 @@
 import fetch from 'node-fetch'
 import config from 'lib/config'
+// import {asSeconds} from 'pomeranian-durations'
 
-// const {asSeconds} = require('pomeranian-durations')
-
-const {YOUTUBE_API_KEY: key} = config
+const {YOUTUBE_API_KEY} = config
 
 export default async function handler(req, res) {
 	const ytid = req.query.ytid
 
 	try {
 		// Make sure we have what we need.
-		if (!key) throw new Error('A YOUTUBE_KEY in your .env file is required')
+		if (!YOUTUBE_API_KEY) throw new Error('A YOUTUBE_KEY in your .env file is required')
 		if (!ytid) throw new Error('A ytid query parameter is required')
 
 		// Fetch from the YouTube Data API. Key is from Google Cloud console credentials.
-		const data = await fetch(
-			`https://www.googleapis.com/youtube/v3/videos?part=status,contentDetails,snippet&id=${ytid}&key=${key}`
-		)
-		const json = await data.json()
+		const {items} = await fetch(
+			`https://www.googleapis.com/youtube/v3/videos?part=status,contentDetails,snippet&id=${ytid}&key=${YOUTUBE_API_KEY}`
+		).then((res) => res.json())
 
 		// It does not throw with no results, so we check here.
-		if (json.items.length === 0) {
-			return res.status(404).json({message: 'YouToube video not found'})
-		}
+		if (items.length === 0) return res.status(404).json({message: 'YouToube video not found'})
 
 		// Serialize and return.
-		res.status(200).json(serialize(json.items[0]))
+		res.status(200).json(serialize(items[0]))
 	} catch (err) {
 		res.status(500).json({message: err.message})
 	}
