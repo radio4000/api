ab0f88111d78d9373a9cfceacfd4d5e0c47044cf
oskarrough <oskar@rough.dk>
2026-01-29 10:32:22 +0100

    Fix /api/v2/embed endpoint

--- src/v2/embed.ts
+++ src/v2/embed.ts
@@ -25,34 +25,61 @@ app.get('/', (c) => {
 })
 
 const getIframe = (slug: string, baseUrl: string) => {
-	return `<!DOCTYPE html>
+	return `<!doctype html>
 <html>
 	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width">
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width" />
 		<title>@${slug} | Radio4000</title>
 		<meta name="description" content="${slug}@r4-player">
 		<style>
-			html, body, radio4000-player {
-				height: 100% !important;
-			}
-			body {
+			html,
+			body,
+			r4-player {
+				min-height: 100%;
+				min-width: 100%;
+				display: flex;
 				margin: 0;
+			}
+			r4-player radio4000-player {
+				flex-grow: 1;
+				height: 100%;
 			}
 		</style>
 	</head>
 	<body>
+		<r4-player></r4-player>
 
-		<r4-app
-			href="${baseUrl}/api/v2/embed/"
-			single-channel="true"
-			channel=${slug}
-			cdn
-		></r4-app>
+		<script type="module">
+			import "https://fastly.jsdelivr.net/npm/@radio4000/components@0.2.17/dist/r4.js";
+			import { sdk } from "https://fastly.jsdelivr.net/npm/@radio4000/sdk/+esm";
 
-		<script async type="module" src="https://cdn.jsdelivr.net/npm/@radio4000/components@latest/dist/r4.js"><\/script>
-		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@radio4000/components@latest/dist/r4.css" />
+			function createImage(id) {
+				const baseUrl = "https://res.cloudinary.com/radio4000/image/upload";
+				const size = 250;
+				const dimensions = \`w_\${size},h_\${size}\`;
+				const crop = "c_thumb,q_60";
+				return \`\${baseUrl}/\${dimensions},\${crop},fl_awebp/\${id}.webp\`;
+			}
 
+			(async ({ slug = "" }) => {
+				if (!slug) return;
+				const { data: channel } = await sdk.channels.readChannel(slug);
+				const { data: tracks } = await sdk.channels.readChannelTracks(slug);
+				const $app = document.querySelector("r4-player");
+				$app.setAttribute("href", "${baseUrl}");
+				$app.setAttribute("name", channel.name);
+				$app.setAttribute("image", createImage(channel.image));
+				$app.tracks = tracks
+					.map((track) => {
+						return {
+							...track,
+							body: track.description,
+						};
+					})
+					.reverse();
+			})({ slug: "${slug}" });
+		<\/script>
 	</body>
 </html>`
 }
